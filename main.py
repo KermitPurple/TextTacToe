e7c76d74a3d33b9769e1039260ea2ce0882a041e
Shane McDonough <shanemcdman@gmail.com>
2020-12-24 04:00:40 -0800

    Comment code

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ class TextTacToe:
         """
         Print the tic tac toe board and its contents
         """
-        for i in range(self.size.y):
-            for j in range(self.size.x):
-                print(self.board[i][j] + (' |' if j < self.size.x - 1 else ''), end='')
-            if i < self.size.y - 1:
-                print('\n--+--+--')
+        for i in range(self.size.y): # cycle through y coords
+            for j in range(self.size.x): # cycle through x coords
+                print(self.board[i][j] + (' |' if j < self.size.x - 1 else ''), end='') # print values and vertical lines in between
+            if i < self.size.y - 1: # if this isn't the last loop
+                print('\n--+--+--') # print horizontal lines between values
 
     @staticmethod
     def get_matrix(x: int, y: int) -> [[int]]:
@@ -40,4 +40,3 @@ class TextTacToe:
 if __name__ == '__main__':
     ttt = TextTacToe()
     ttt.print()
-    c = Coord(10, 5)
